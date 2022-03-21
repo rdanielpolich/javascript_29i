702d810c5df00f6ff5f247c6b9047eb03ad7938e
rdanielpolich <rdanielpolich@gmail.com>
2022-03-21 17:23:51 -0300

    primera clase js

---
+++ js/fundamentos/01- variables-operadores.js
@@ -0,0 +1,92 @@
+            //código de Js
+
+      //alert("Bienvenidos a Javascript");
+
+      //mensajes en la consola
+      console.log("Bienvenidos a Javascript");
+      console.log(5 + 3);
+
+      //mostrar en el navegador
+      document.write("Es un gusto saludarte en este curso");
+
+      //Variables
+      //las variables son espacios en memoria que usamos para guardar datos
+
+      //declara: creación de la variable
+      //inicializa: asignación de un valor
+
+      var nombre
+      nombre="pepito"
+
+      let apellido = "Gonzalez";
+
+      const pi = 3.14;
+
+      apellido="Perez"
+
+
+
+      //tipos de datos
+
+      //string
+      let cliente="Fabrizio"
+      let alumno="Roberto Gonzalez"
+      let mensaje="hola mundo soy feliz"
+
+      //number
+      let numero1=34
+      let numero2=-52
+      let numero3=56.89
+      
+      //Booleanos
+      let comprar = true
+      let comer = false
+
+      //null
+      let billetera = null
+
+      //undefined
+
+      let caja;
+
+      //array o arreglos
+      let arreglo= ["Pablo",45,false]
+      let alumnos = ["Jose","Alberto","Florinda"]
+
+      //object u objetos
+
+      let persona = {
+            nombre:"Josefina",
+            apellido:"Mandarino",
+            edad:33
+      }
+      
+      //Operadores
+
+      //Aritmeticos
+      let suma= numero1 + numero2;
+      let resta = numero1 - numero2;
+      let producto = numero1 * numero3;
+      let division = numero1 / numero2;
+      let resto = numero1 % numero2
+
+      //Unitarios
+      ++numero2
+      --numero2
+
+      //relacionales
+      
+      console.log(numero1 === numero2)
+      console.log(numero1 >= numero2)
+      console.log(numero1 <= numero2)
+      console.log(numero1 != numero2)
+
+      console.log("el resultado de la suma es "+ suma)
+
+      console.log("el resultado de la suma es ", resta)
+
+
+//Negacion
+
+let activado = true
+console.log(!activado)
