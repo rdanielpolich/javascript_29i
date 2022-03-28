674c84f235e2cb77428664710a8b22a7b040798f
rdanielpolich <rdanielpolich@gmail.com>
2022-03-28 15:52:51 -0300

    segundo commit

--- js/fundamentos/01- variables-operadores.js
+++ js/fundamentos/01- variables-operadores.js
@@ -1,92 +1,89 @@
-            //código de Js
+//código de Js
 
-      //alert("Bienvenidos a Javascript");
+//alert("Bienvenidos a Javascript");
 
-      //mensajes en la consola
-      console.log("Bienvenidos a Javascript");
-      console.log(5 + 3);
+//mensajes en la consola
+console.log("Bienvenidos a Javascript");
+console.log(5 + 3);
 
-      //mostrar en el navegador
-      document.write("Es un gusto saludarte en este curso");
+//mostrar en el navegador
+document.write("Es un gusto saludarte en este curso");
 
-      //Variables
-      //las variables son espacios en memoria que usamos para guardar datos
+//Variables
+//las variables son espacios en memoria que usamos para guardar datos
 
-      //declara: creación de la variable
-      //inicializa: asignación de un valor
+//declara: creación de la variable
+//inicializa: asignación de un valor
 
-      var nombre
-      nombre="pepito"
+var nombre;
+nombre = "pepito";
 
-      let apellido = "Gonzalez";
+let apellido = "Gonzalez";
 
-      const pi = 3.14;
+const pi = 3.14;
 
-      apellido="Perez"
+apellido = "Perez";
 
+//tipos de datos
 
+//string
+let cliente = "Fabrizio";
+let alumno = "Roberto Gonzalez";
+let mensaje = "hola mundo soy feliz";
 
-      //tipos de datos
+//number
+let numero1 = 34;
+let numero2 = -52;
+let numero3 = 56.89;
 
-      //string
-      let cliente="Fabrizio"
-      let alumno="Roberto Gonzalez"
-      let mensaje="hola mundo soy feliz"
+//Booleanos
+let comprar = true;
+let comer = false;
 
-      //number
-      let numero1=34
-      let numero2=-52
-      let numero3=56.89
-      
-      //Booleanos
-      let comprar = true
-      let comer = false
+//null
+let billetera = null;
 
-      //null
-      let billetera = null
+//undefined
 
-      //undefined
+let caja;
 
-      let caja;
+//array o arreglos
+let arreglo = ["Pablo", 45, false];
+let alumnos = ["Jose", "Alberto", "Florinda"];
 
-      //array o arreglos
-      let arreglo= ["Pablo",45,false]
-      let alumnos = ["Jose","Alberto","Florinda"]
+//object u objetos
 
-      //object u objetos
+let persona = {
+  nombre: "Josefina",
+  apellido: "Mandarino",
+  edad: 33,
+};
 
-      let persona = {
-            nombre:"Josefina",
-            apellido:"Mandarino",
-            edad:33
-      }
-      
-      //Operadores
+//Operadores
 
-      //Aritmeticos
-      let suma= numero1 + numero2;
-      let resta = numero1 - numero2;
-      let producto = numero1 * numero3;
-      let division = numero1 / numero2;
-      let resto = numero1 % numero2
+//Aritmeticos
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let producto = numero1 * numero3;
+let division = numero1 / numero2;
+let resto = numero1 % numero2;
 
-      //Unitarios
-      ++numero2
-      --numero2
+//Unitarios
+++numero2;
+--numero2;
 
-      //relacionales
-      
-      console.log(numero1 === numero2)
-      console.log(numero1 >= numero2)
-      console.log(numero1 <= numero2)
-      console.log(numero1 != numero2)
+//relacionales
 
-      console.log("el resultado de la suma es "+ suma)
+console.log(numero1 === numero2);
+console.log(numero1 >= numero2);
+console.log(numero1 <= numero2);
+console.log(numero1 != numero2);
 
-      console.log("el resultado de la suma es ", resta)
+console.log("el resultado de la suma es " + suma);
 
+console.log("el resultado de la suma es ", resta);
 
 //Negacion
 
-let activado = true
-console.log(!activado)
+let activado = true;
+console.log(!activado);
